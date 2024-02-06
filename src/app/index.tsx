b9a091357160e183b01da478515550d5ceedde99
Suami Rocha <suamirochati@gmail.com>
2024-02-06 12:17:14 -0300

    fix: using state to dynamically select categories

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { View, FlatList } from "react-native";
 import { CATEGORIES } from "@/utils/data/products";
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
+
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory)
+  }
   return (
     <View className="flex-1 pt-8">
       <Header title="Faça seu pedido" cartQuantityItems={5} />
@@ -10,7 +17,13 @@ export default function Home() {
       <FlatList
         data={CATEGORIES}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <CategoryButton title={item} />}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
         horizontal
         className="max-w-10 mt-5"
         showsHorizontalScrollIndicator={false}
