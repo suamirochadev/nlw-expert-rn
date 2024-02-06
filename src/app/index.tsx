5d7878e45026fd34c50de7e78f37e7d39cfb0083
Suami Rocha <suamirochati@gmail.com>
2024-02-06 11:47:02 -0300

    feat: category buttons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,17 @@
 import { View } from "react-native";
 import { Header } from "@/components/header";
+import { CategoryButton } from "@/components/category-button";
 
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Header title="Faça seu pedido" cartQuantityItems = {5} />
+      <Header title="Faça seu pedido" cartQuantityItems={5} />
+
+      <View className="flex-row gap-4">
+        <CategoryButton title="Lanche do dia" isSelected />
+        <CategoryButton title="Lanche do dia" />
+        <CategoryButton title="Lanche do dia" />
+      </View>
     </View>
   );
 }
