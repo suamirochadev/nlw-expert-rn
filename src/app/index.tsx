41cb8fea79ecc17342b58fa53a2e6240af5a6d55
Suami Rocha <suamirochati@gmail.com>
2024-02-06 12:00:33 -0300

    fix: linking categories to the database

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,21 @@
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
+import { CATEGORIES } from "@/utils/data/products";
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
-
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
       <Header title="Faça seu pedido" cartQuantityItems={5} />
 
-      <View className="flex-row gap-4">
-        <CategoryButton title="Lanche do dia" isSelected />
-        <CategoryButton title="Lanche do dia" />
-        <CategoryButton title="Lanche do dia" />
-      </View>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <CategoryButton title={item} />}
+        horizontal
+        className="max-w-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 10, paddingHorizontal: 20 }}
+      />
     </View>
   );
 }
