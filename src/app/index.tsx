d5ffdab5ecd3912fa5ddf88b8b7ed4e9bc062f1e
Suami Rocha <suamirochati@gmail.com>
2024-02-06 11:23:42 -0300

    fisrt commit

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Header } from "@/components/header";
+
+export default function Home() {
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cartQuantityItems = {5} />
+    </View>
+  );
+}
