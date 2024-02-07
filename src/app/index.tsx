efb09ca61e42f9637f96e6505249b428e2c1b7ab
Suami Rocha <suamirochati@gmail.com>
2024-02-07 11:52:29 -0300

    fix: error rendering home and product page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,34 +1,68 @@
-import { useState } from "react";
-import { View, FlatList } from "react-native";
-import { CATEGORIES } from "@/utils/data/products";
+import { useState, useRef } from "react";
+import { View, FlatList, SectionList, Text } from "react-native";
+import { CATEGORIES, MENU } from "@/utils/data/products";
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
+import { Product } from "@/components/product";
+import { Link } from "expo-router";
 
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0]);
+  const sectionListRef = useRef<SectionList>(null);
 
   function handleCategorySelect(selectedCategory: string) {
-    setCategory(selectedCategory)
-  }
-  return (
-    <View className="flex-1 pt-8">
-      <Header title="Faça seu pedido" cartQuantityItems={5} />
+    setCategory(selectedCategory);
+    const sectionIndex = CATEGORIES.findIndex(
+      (category) => category === selectedCategory
+    );
 
-      <FlatList
-        data={CATEGORIES}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => (
-          <CategoryButton
-            title={item}
-            isSelected={item === category}
-            onPress={() => handleCategorySelect(item)}
-          />
-        )}
-        horizontal
-        className="max-w-10 mt-5"
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{ gap: 10, paddingHorizontal: 20 }}
-      />
-    </View>
-  );
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex: sectionIndex,
+        itemIndex: 0,
+      });
+    }
+    return (
+      <View className="flex-1 pt-8">
+        <Header title="Faça seu pedido" cartQuantityItems={2} />
+
+        <FlatList
+          data={CATEGORIES}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <CategoryButton
+              title={item}
+              isSelected={item === category}
+              onPress={() => handleCategorySelect(item)}
+            />
+          )}
+          horizontal
+          className="max-w-10 mt-5"
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ gap: 10, paddingHorizontal: 20 }}
+        />
+
+        <SectionList
+          ref={sectionListRef}
+          sections={MENU}
+          keyExtractor={(item) => item.id}
+          stickySectionHeadersEnabled={false}
+          renderItem={({ item }) => (
+            <Link href={`/product/${item.id}`} asChild>
+              <Product data={item} />
+            </Link>
+          )}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text className="text-xl text-white font-heading mt-8 mb-3">
+              {title}
+            </Text>
+          )}
+          className="flex-1 p-5"
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+        />
+      </View>
+    )
+  }
 }
---
+++ src/app/product/[id].tsx
@@ -0,0 +1,48 @@
+import { Text, View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import { PRODUCTS } from "@/utils/data/products";
+import { Image } from "react-native";
+import { formatCurrency } from "@/utils/functions/format-currency";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
+
+export default function Product() {
+  const { id } = useLocalSearchParams();
+
+  const product = PRODUCTS.filter((item) => item.id === id)[0];
+
+  return (
+    <View className="flex-1">
+      <Image
+        source={product.cover}
+        className="w-full h-52"
+        resizeMode="cover"
+      ></Image>
+      <View className="p-5 mt-8 flex-1">
+        <Text className="text-lime-400 text-2xl font-heading my-2">
+          {formatCurrency(product.price)}
+        </Text>
+        <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+          {product.description}
+        </Text>
+        {product.ingredients.map((ingredient, index) => (
+          <Text
+            key={index}
+            className="text-slate-400 leading-6 font-body text-base"
+          >
+            {"\u2022"}
+            {ingredient}
+          </Text>
+        ))}
+      </View>
+      <View className="p-5 pb-8 gap-5">
+        <Button>
+          <Button.Icon>
+            <Feather name="plus-circle" size={20} />
+          </Button.Icon>
+          <Button.Text>Adicionar ao pedido</Button.Text>
+        </Button>
+      </View>
+    </View>
+  )
+}
