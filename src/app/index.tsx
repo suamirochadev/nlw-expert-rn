73c4f01c4204c468ca5a773910dc0f9c3c043c8d
Suami Rocha <suamirochati@gmail.com>
2024-02-07 23:05:47 -0300

    feat: create state with zustand

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,10 +5,15 @@ import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
 import { Product } from "@/components/product";
 import { Link } from "expo-router";
+import { useStore } from "zustand";
+import { useCartStore } from "@/stores/cart-store";
 
 export default function Home() {
+  const cartStore = useCartStore();
   const [category, setCategory] = useState(CATEGORIES[0]);
   const sectionListRef = useRef<SectionList>(null);
+
+  const cartQuantityItems = cartStore.products.reduce((total, product) => total + product.quantity, 0);
 
   function handleCategorySelect(selectedCategory: string) {
     setCategory(selectedCategory);
@@ -24,45 +29,45 @@ export default function Home() {
       });
     }
   }
-    return (
-      <View className="flex-1 pt-8">
-        <Header title="Faça seu pedido" cartQuantityItems={2} />
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cartQuantityItems={cartQuantityItems} />
 
-        <FlatList
-          data={CATEGORIES}
-          keyExtractor={(item) => item}
-          renderItem={({ item }) => (
-            <CategoryButton
-              title={item}
-              isSelected={item === category}
-              onPress={() => handleCategorySelect(item)}
-            />
-          )}
-          horizontal
-          className="max-w-10 mt-5"
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ gap: 10, paddingHorizontal: 20 }}
-        />
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 10, paddingHorizontal: 20 }}
+      />
 
-        <SectionList
-          ref={sectionListRef}
-          sections={MENU}
-          keyExtractor={(item) => item.id}
-          stickySectionHeadersEnabled={false}
-          renderItem={({ item }) => (
-            <Link href={`/product/${item.id}`} asChild>
-              <Product data={item} />
-            </Link>
-          )}
-          renderSectionHeader={({ section: { title } }) => (
-            <Text className="text-xl text-white font-heading mt-8 mb-3">
-              {title}
-            </Text>
-          )}
-          className="flex-1 p-5"
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ paddingBottom: 100 }}
-        />
-      </View>
-    )
-  }
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      />
+    </View>
+  );
+}
--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,15 +1,24 @@
 import { Text, View } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 import { PRODUCTS } from "@/utils/data/products";
 import { Image } from "react-native";
 import { formatCurrency } from "@/utils/functions/format-currency";
 import { Button } from "@/components/button";
 import { Feather } from "@expo/vector-icons";
 
+import { useCartStore } from "@/stores/cart-store";
+import { LinkButton } from "@/components/link-button";
+
 export default function Product() {
   const { id } = useLocalSearchParams();
-
+  const cartStore = useCartStore();
+  const navigation = useNavigation();
   const product = PRODUCTS.filter((item) => item.id === id)[0];
+
+  function handleAddToCart() {
+    cartStore.add(product);
+    navigation.goBack();
+  }
 
   return (
     <View className="flex-1">
@@ -30,19 +39,23 @@ export default function Product() {
             key={index}
             className="text-slate-400 leading-6 font-body text-base"
           >
-            {"\u2022"}
-            {ingredient}
+            {"\u2022"} {ingredient}
           </Text>
         ))}
       </View>
       <View className="p-5 pb-8 gap-5">
-        <Button>
+        <Button onPress={handleAddToCart}>
           <Button.Icon>
             <Feather name="plus-circle" size={20} />
           </Button.Icon>
           <Button.Text>Adicionar ao pedido</Button.Text>
         </Button>
+        <LinkButton
+          title="Voltar ao cardapio"
+          href="/"
+          className="text-lime-400 text-center text-base font-body"
+        />
       </View>
     </View>
-  )
+  );
 }
